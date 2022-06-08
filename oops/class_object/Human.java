8be0e9d4269f05fed4d11236a707b26d2f4f1ab2
jesusb2b <39941574+berabappaditya@users.noreply.github.com>
2022-06-08 23:05:52 +0530

    class_obj1

---
+++ oops/class_object/Human.java
@@ -0,0 +1,15 @@
+package oops.class_object;
+
+public class Human {
+    int age;
+    String type;
+    String gender;
+
+    Human() {
+    }
+
+    Human(int age, String type, String gender) {
+
+    }
+
+}
